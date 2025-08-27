e7c0263f2c384a8e7857dedf874eb6a315f8eace
truepaklen <truepaklen@gmail.com>
2025-08-27 13:48:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import sys
 print("✅ Python version:", sys.version)
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from config import BOT_TOKEN
 
-def start(update, context):
-    update.message.reply_text("Привет! Я бот.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот.")
 
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-
-updater.start_polling()
-updater.idle()
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
